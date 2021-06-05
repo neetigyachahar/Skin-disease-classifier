9d07bac36f80275904fd0f80ef15b1b48c4ec3c9
Neetigya Chahar <neetigyachahar784@gmail.com>
2021-06-05 10:19:06 +0530

    code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Skin disease Prediction using Convolutional neural networks 
+
+An Image processing project by 
+Avinash 18BCE0363
+Shubham 18BCE0356
+Neetigya 18BCE0794
+
+
+With ever growing pollution and climate disruptions, our way of living and lifestyle is giving rise to many skin related diseases. For some of which early and accurate detection becomes utmost necessary in order to save the patient. Given the availability of skin doctors in hospitals and huge expenditure associated with appointments, it becomes almost impossible for a poor to afford even a diagnosis related to a skin disease. The project aims at providing skin disease prediction technology made using deep learning. The project aims to preprocess the images, perform data augmentation and finally make use of deep learning technology to train itself with the various skin images. Then it is tested with the test set to check for accuracy. Finally a web implementation is developed to deploy our model.
+
+
+## Demo
+![result](./screenshots/result.jpg)
+
+## video
+https://drive.google.com/file/d/1h3l7puX44rmxC8BYw-G3uDy_uWltWEdT/view?usp=sharing
---
+++ augment.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 07:20:10 2021
+
+@author: AVINASH
+"""
+
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+# load the image
+print("\n\nIMAGE PROCESSING PROJECT | SKIN DISEASE DETECTION | IMAGE AUGMENTATION\n")
+print("")
+img = load_img('melanoma.jpg')
+print("original image of melanoma.jpg")
+pyplot.imshow(img)
+pyplot.show()
+# convert to numpy array
+data = img_to_array(img)
+# expand dimension to one sample
+samples = expand_dims(data, 0)
+# create image data augmentation generator
+datagen = ImageDataGenerator(width_shift_range=[-200,200])
+# prepare iterator
+it = datagen.flow(samples, batch_size=1)
+# generate samples and plot
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# generate batch of images
+	batch = it.next()
+	# convert to unsigned integers for viewing
+	image = batch[0].astype('uint8')
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+print("\n\nhorizontal shift")
+pyplot.show()
+
+
+datagen = ImageDataGenerator(rotation_range=90)
+# prepare iterator
+it = datagen.flow(samples, batch_size=1)
+# generate samples and plot
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# generate batch of images
+	batch = it.next()
+	# convert to unsigned integers for viewing
+	image = batch[0].astype('uint8')
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+print("\n\nRandom rotation")
+pyplot.show()
+
+
+
+datagen = ImageDataGenerator(brightness_range=[1,2.0])
+# prepare iterator
+it = datagen.flow(samples, batch_size=1)
+# generate samples and plot
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# generate batch of images
+	batch = it.next()
+	# convert to unsigned integers for viewing
+	image = batch[0].astype('uint8')
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+print("\n\nBrightness Augmentation")
+pyplot.show()
+
+
+datagen = ImageDataGenerator(zoom_range=[0.5,1.0])
+# prepare iterator
+it = datagen.flow(samples, batch_size=1)
+# generate samples and plot
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# generate batch of images
+	batch = it.next()
+	# convert to unsigned integers for viewing
+	image = batch[0].astype('uint8')
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+print("\n\nZoom Augmentation")
+pyplot.show()
